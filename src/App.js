1cd50123fb8f8df72bfb6b81b7cdd327092064e7
lengebre <lengelbre84@gmail.com>
2020-12-25 14:47:12 -0500

    Removed additional files no longer in use and reorganized features

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <h1 className="app_header">Gmail Clone</h1>
+    </div>
+  );
 }
 
 export default App;
