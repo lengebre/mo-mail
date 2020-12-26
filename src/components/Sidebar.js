8d546a29fc6ae9dbbed9bc689b7f00c9275be5cb
lengebre <lengelbre84@gmail.com>
2020-12-26 15:58:28 -0500

    Setup Section styling

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,16 @@
 import { Button, IconButton } from "@material-ui/core";
 import "./Sidebar.css";
-import AddIcon from "@material-ui/icons/Add";
+import {
+  Add,
+  Inbox,
+  StarRate,
+  WatchLater,
+  LabelImportant,
+  Send,
+  InsertDriveFile,
+  ExpandMore,
+} from "@material-ui/icons";
 import SidebarOption from "./SidebarOption";
-import InboxIcon from "@material-ui/icons/Inbox";
-import StarRateIcon from "@material-ui/icons/StarRate";
-import WatchLaterIcon from "@material-ui/icons/WatchLater";
-import LabelImportantIcon from "@material-ui/icons/LabelImportant";
-import SendIcon from "@material-ui/icons/Send";
-import InsertDriveFileIcon from "@material-ui/icons/InsertDriveFile";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import PersonIcon from "@material-ui/icons/Person";
 import DuoIcon from "@material-ui/icons/Duo";
 import PhoneIcon from "@material-ui/icons/Phone";
@@ -17,21 +19,16 @@ function Sidebar() {
   return (
     <div className="sidebar">
       <Button>
-        <AddIcon fontSize="16px" />
+        <Add fontSize="16px" />
         Compose
       </Button>
-      <SidebarOption
-        Icon={InboxIcon}
-        title="Inbox"
-        number={77}
-        selected={true}
-      />
-      <SidebarOption Icon={StarRateIcon} title="Starred" number={5} />
-      <SidebarOption Icon={WatchLaterIcon} title="Snoozed" number={5} />
-      <SidebarOption Icon={LabelImportantIcon} title="Important" number={5} />
-      <SidebarOption Icon={SendIcon} title="Sent" number={5} />
-      <SidebarOption Icon={InsertDriveFileIcon} title="Drafts" number={5} />
-      <SidebarOption Icon={ExpandMoreIcon} title="More" />
+      <SidebarOption Icon={Inbox} title="Inbox" number={77} selected={true} />
+      <SidebarOption Icon={StarRate} title="Starred" number={5} />
+      <SidebarOption Icon={WatchLater} title="Snoozed" number={5} />
+      <SidebarOption Icon={LabelImportant} title="Important" number={5} />
+      <SidebarOption Icon={Send} title="Sent" number={5} />
+      <SidebarOption Icon={InsertDriveFile} title="Drafts" number={5} />
+      <SidebarOption Icon={ExpandMore} title="More" />
 
       <div className="sidebar__footer">
         <div className="sidebar__footerIcons">
